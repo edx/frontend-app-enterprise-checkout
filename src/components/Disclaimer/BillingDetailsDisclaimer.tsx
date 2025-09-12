3399f56af181ac5e31eb7056c8069decaa2637fa
Hamzah Ullah <hamzahullah@yahoo.com>
2025-09-12 10:54:15 -0500

    chore: additional cleanup

--- src/components/Disclaimer/BillingDetailsDisclaimer.tsx
+++ src/components/Disclaimer/BillingDetailsDisclaimer.tsx
@@ -9,7 +9,7 @@ const BillingDetailsDisclaimer = () => {
   const { yearlySubscriptionCostForQuantity } = usePurchaseSummaryPricing();
   const trialEndDate = dayjs().add(SUBSCRIPTION_TRIAL_LENGTH_DAYS, 'days').format(DATE_FORMAT);
   return (
-    <p>
+    <p className="font-weight-light">
       <FormattedMessage
         id="checkout.billingDetails.disclaimer.beforeBreak"
         defaultMessage="By signing up for a subscription or starting a {trialDays}-day free trial, you authorize us to charge your card on file {price}/year USD. Your subscription will automatically renew each year until you cancel. Cancel anytime by visiting the Subscription Management page. Refunds and credits are not available. Cancelling a subscription stops the annual recurring subscription charge, but does not refund the transaction for the current billing period. You can continue to access the subscription until the end of the then-current billing period."
--- src/components/FormFields/StripeFormFields.tsx
+++ src/components/FormFields/StripeFormFields.tsx
@@ -4,16 +4,39 @@ import { AddressElement, PaymentElement } from '@stripe/react-stripe-js';
 import { FieldContainer } from '@/components/FieldContainer';
 import { StripeProvider } from '@/components/StripeProvider';
 
+const StripeAddressTitle = () => (
+  <h3 className="mb-3">
+    <FormattedMessage
+      id="billingDetailsPage.stripeFormFields.billingAddress.title"
+      defaultMessage="Customer billing information"
+      description="Stripe form billing address information title"
+    />
+  </h3>
+);
+
+const StripePaymentTitle = () => (
+  <>
+    <h3>
+      <FormattedMessage
+        id="billingDetailsPage.stripeFormFields.billingPayment.title"
+        defaultMessage="Billing information"
+        description="Stripe form billing payment information title"
+      />
+    </h3>
+    <h4 className="font-weight-light">
+      <FormattedMessage
+        id="billingDetailsPage.stripeFormFields.billingPayment.subtitle"
+        defaultMessage="By providing your card information, you allow edX to charge your card for future payments in accordance with their terms."
+        description="Description text explaining the billing payment infomration subtitle"
+      />
+    </h4>
+  </>
+);
+
 const StripeFormFields = () => (
   <StripeProvider>
     <FieldContainer>
-      <h3>
-        <FormattedMessage
-          id="billingDetailsPage.stripeFormFields.billingAddress.title"
-          defaultMessage="Customer billing information"
-          description="Stripe form billing address information title"
-        />
-      </h3>
+      <StripeAddressTitle />
       <AddressElement
         options={{
           mode: 'billing',
@@ -21,23 +44,9 @@ const StripeFormFields = () => (
       />
     </FieldContainer>
     <FieldContainer>
-      <h3>
-        <FormattedMessage
-          id="billingDetailsPage.stripeFormFields.billingPayment.title"
-          defaultMessage="Billing information"
-          description="Stripe form billing payment information title"
-        />
-      </h3>
-      <h3 className="font-weight-light">
-        <FormattedMessage
-          id="billingDetailsPage.stripeFormFields.billingPayment.title"
-          defaultMessage="Enter in the number of licenses you want to purchase."
-          description="Description text explaining the licenses field purpose"
-        />
-      </h3>
+      <StripePaymentTitle />
       <PaymentElement />
     </FieldContainer>
-
   </StripeProvider>
 );
 
