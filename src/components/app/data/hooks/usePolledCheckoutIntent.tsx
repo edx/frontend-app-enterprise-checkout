73970b50e38b6b281c12f6ccc7cdaff5dcdd0601
Hamzah Ullah <hamzahullah@yahoo.com>
2025-09-30 13:04:45 -0400

    chore: invalidate queries

--- src/components/app/data/hooks/usePolledCheckoutIntent.tsx
+++ src/components/app/data/hooks/usePolledCheckoutIntent.tsx
@@ -1,16 +1,28 @@
-import { queryOptions, useQuery } from '@tanstack/react-query';
+import { AppContext } from '@edx/frontend-platform/react';
+import { queryOptions, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useContext } from 'react';
 
 import useCheckoutIntent from '@/components/app/data/hooks/useCheckoutIntent';
-import { queryCheckoutIntent } from '@/components/app/data/queries/queries';
+import { queryBffContext, queryBffSuccess, queryCheckoutIntent } from '@/components/app/data/queries/queries';
 
 const usePolledCheckoutIntent = () => {
   const { data: checkoutIntent } = useCheckoutIntent();
+  const queryClient = useQueryClient();
+  const { authenticatedUser }: AppContextValue = useContext(AppContext);
 
   const polledCheckoutIntent = useQuery(
     queryOptions({
       ...queryCheckoutIntent(checkoutIntent!.id),
       refetchInterval: (queryMetadata) => {
         if (queryMetadata.state.data?.state === 'fulfilled') {
+          Promise.all(
+            [queryClient.invalidateQueries({
+              queryKey: queryBffContext(authenticatedUser.id).queryKey,
+            }),
+            queryClient.invalidateQueries({
+              queryKey: queryBffSuccess(authenticatedUser.id).queryKey,
+            })],
+          );
           return false;
         }
         return 5000;
