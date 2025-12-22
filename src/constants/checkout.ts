10e6d994a144a705cfffc11781c7f8fa52ab4ebb
Marlon Keating <mkeating@2u.com>
2025-12-22 17:54:41 +0000

    feat: add companyName pre-submit validation

--- src/constants/checkout.ts
+++ src/constants/checkout.ts
@@ -31,6 +31,7 @@ export type FieldErrorCodes = {
   enterpriseSlug: 'invalid_format' | 'existing_enterprise_customer' | 'slug_reserved' | 'incomplete_data';
   quantity: 'invalid_format' | 'range_exceeded' | 'incomplete_data';
   stripePriceId: 'invalid_format' | 'does_not_exist' | 'incomplete_data';
+  companyName: 'existing_enterprise_customer';
 };
 
 export const CheckoutErrorMessagesByField: { [K in keyof FieldErrorCodes]: Record<FieldErrorCodes[K], string> } = {
@@ -42,7 +43,7 @@ export const CheckoutErrorMessagesByField: { [K in keyof FieldErrorCodes]: Recor
   enterpriseSlug: {
     invalid_format: 'Only alphanumeric lowercase characters and hyphens are allowed.',
     // EXISTING_ENTERPRISE_CUSTOMER_FOR_ADMIN uses the same error code on the backend
-    existing_enterprise_customer: 'The slug conflicts with an existing customer.',
+    existing_enterprise_customer: 'URL is already in use.',
     slug_reserved: 'The slug is currently reserved by another user.',
     incomplete_data: 'Not enough parameters were given.',
   },
@@ -55,6 +56,9 @@ export const CheckoutErrorMessagesByField: { [K in keyof FieldErrorCodes]: Recor
     invalid_format: 'Must be a non-empty string.',
     does_not_exist: 'This stripe_price_id has not been configured.',
     incomplete_data: 'Not enough parameters were given.',
+  },
+  companyName: {
+    existing_enterprise_customer: 'This company already has an edX account. Please contact support to request access or modify the existing account',
   },
 };
 
@@ -213,7 +217,19 @@ export const AccountDetailsSchema = (
     .max(
       constraints?.companyName?.maxLength ?? 255,
       `Maximum ${constraints?.companyName?.maxLength ?? 255} characters`,
-    ),
+    )
+    .superRefine(async (companyName, ctx) => {
+      const { isValid, validationDecisions } = await validateFieldDetailed(
+        'companyName',
+        companyName,
+      );
+      if (!isValid && validationDecisions?.companyName) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: serverValidationError('companyName', validationDecisions, CheckoutErrorMessagesByField),
+        });
+      }
+    }),
   enterpriseSlug: z.string().trim()
     .min(
       constraints?.enterpriseSlug?.minLength ?? 1,
