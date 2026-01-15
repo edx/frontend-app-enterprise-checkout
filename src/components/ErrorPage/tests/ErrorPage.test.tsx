7289b4eda1585c663a5c276d482d4a8e3341592c
Marlon Keating <mkeating@2u.com>
2026-01-15 15:41:39 +0000

    test: add non stack trace test case

--- src/components/ErrorPage/tests/ErrorPage.test.tsx
+++ src/components/ErrorPage/tests/ErrorPage.test.tsx
@@ -110,6 +110,15 @@ describe('ErrorPage', () => {
     validateText('500 Internal Server Error');
   });
 
+    it('displays error message from useRouteError when error object is returned', () => {
+    (useRouteError as jest.Mock).mockImplementation(() => {
+      return new Error('Hook failed');
+    });
+    renderComponent();
+    validateText("We're sorry, something went wrong");
+    validateText('Hook failed');
+  });
+
   it('displays error message and stack trace from useRouteError when error object is returned', () => {
     (useRouteError as jest.Mock).mockImplementation(() => {
       const error = new Error('Hook failed');
