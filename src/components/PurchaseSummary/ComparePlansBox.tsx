e544f5174ec3dcebb9651100778e641041e6238e
rkumarsingh-sonata-crypto <rkumarsingh-sonata@2u.com>
2026-04-28 03:28:10 +0000

    chore: fix lint issues

--- src/components/PurchaseSummary/ComparePlansBox.tsx
+++ src/components/PurchaseSummary/ComparePlansBox.tsx
@@ -3,9 +3,25 @@ import { FormattedMessage } from '@edx/frontend-platform/i18n';
 import { Card, Hyperlink } from '@openedx/paragon';
 import React from 'react';
 
+const renderStrong = (chunks: React.ReactNode) => <strong>{chunks}</strong>;
+
 const ComparePlansBox = () => {
   const { COMPARE_ENTERPRISE_PLANS_URL } = getConfig();
   const comparePlansUrl = COMPARE_ENTERPRISE_PLANS_URL || 'https://business.edx.org/course-library-compare-plans/';
+  const comparePlansLink = (
+    <Hyperlink
+      destination={comparePlansUrl}
+      target="_blank"
+      showLaunchIcon={false}
+      role="link"
+    >
+      <FormattedMessage
+        id="checkout.purchaseSummary.comparePlans.link"
+        defaultMessage="Compare plans."
+        description="Link text for compare plans"
+      />
+    </Hyperlink>
+  );
 
   return (
     <Card className="bg-light-300 rounded border border-light-400" style={{ width: 'min(100%, 401px)', minHeight: '120px', padding: '32px' }}>
@@ -16,21 +32,8 @@ const ComparePlansBox = () => {
             defaultMessage="<strong>Not sure which plan is right for you? </strong>{comparePlansLink}"
             description="Prompt and link to compare plans in essentials purchase summary"
             values={{
-              strong: (chunks: React.ReactNode) => <strong>{chunks}</strong>,
-              comparePlansLink: (
-                <Hyperlink
-                  destination={comparePlansUrl}
-                  target="_blank"
-                  showLaunchIcon={false}
-                  role="link"
-                >
-                  <FormattedMessage
-                    id="checkout.purchaseSummary.comparePlans.link"
-                    defaultMessage="Compare plans."
-                    description="Link text for compare plans"
-                  />
-                </Hyperlink>
-              ),
+              strong: renderStrong,
+              comparePlansLink,
             }}
           />
         </p>
