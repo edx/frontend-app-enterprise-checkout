206c55fc4d5778b9a16aaedffb6ad8938a8b3916
tsunkara-sonata <tsunkara-sonata@2u.com>
2026-08-12 12:52:55 +0000

    feat: added linkout icon for contact support

--- src/components/billing-details-pages/ContactSupport/ContactSupport.tsx
+++ src/components/billing-details-pages/ContactSupport/ContactSupport.tsx
@@ -1,8 +1,8 @@
 import { getConfig } from '@edx/frontend-platform/config';
 import { FormattedMessage } from '@edx/frontend-platform/i18n';
-import { Hyperlink } from '@openedx/paragon';
 
 import { useCheckoutIntent } from '@/components/app/data';
+import { ExternalLink } from '@/components/ExternalLink';
 import EVENT_NAMES from '@/constants/events';
 import { sendEnterpriseCheckoutTrackingEvent } from '@/utils/common';
 
@@ -10,31 +10,29 @@ const ContactSupport = () => {
   const { CONTACT_SUPPORT_URL } = getConfig();
   const { data: checkoutIntent } = useCheckoutIntent();
   return (
-    <div className="text-left w-50 mx-auto">
+    <div className="text-left w-75 mx-auto">
       <p className="text-muted mb-0">
         <FormattedMessage
           id="checkout.orderDetails.needHelp"
-          defaultMessage="For questions about your subscription or our cancellation procedures, please {contactSupport}"
+          defaultMessage="For questions about your subscription or our{lineBreak}cancellation procedures, please {contactSupport}"
           description="Help text with link to contact support"
           values={{
+            lineBreak: <span style={{ display: 'block' }} />,
             contactSupport: (
-              <Hyperlink
-                destination={CONTACT_SUPPORT_URL}
-                target="_blank"
-                rel="noopener noreferrer"
+              <ExternalLink
+                href={CONTACT_SUPPORT_URL}
                 onClick={() => sendEnterpriseCheckoutTrackingEvent({
                   checkoutIntentId: checkoutIntent?.id ?? null,
                   checkoutIntentUuid: checkoutIntent?.uuid ?? null,
                   eventName: EVENT_NAMES.SUBSCRIPTION_CHECKOUT.CONTACT_SUPPORT_LINK_CLICKED,
                 })}
-                showLaunchIcon={false}
               >
                 <FormattedMessage
                   id="checkout.orderDetails.contactSupport"
                   defaultMessage="contact support"
                   description="Link text for contacting support"
                 />
-              </Hyperlink>
+              </ExternalLink>
             ),
           }}
         />
